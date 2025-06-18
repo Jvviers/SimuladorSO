267ff24c8628389520e0283a6fe6635cf813e7e1
FelipeM <fmartinez19@alumnos.utalca.cl>
2025-06-17 22:02:38 -0400

    Se agrega algoritmo RR al planificador

--- src/core/scheduler.js
+++ src/core/scheduler.js
@@ -1,12 +1,15 @@
 import { Estado } from './process.js';
 
 export class Planificador {
-  constructor(procesos, memoria, algoritmo = 'SJF') {
-    this.procesos   = procesos;
-    this.memoria    = memoria;
-    this.algoritmo  = algoritmo;
-    this.tActual    = 0;
-    this.enEjecucion = null;
+  constructor(procesos, memoria, algoritmo = 'SJF', quantum = null) {
+    this.procesos     = procesos;
+    this.memoria      = memoria;
+    this.algoritmo    = algoritmo;
+    this.quantum      = quantum;
+    this.tActual      = 0;
+    this.enEjecucion  = null;
+    this.rrCola       = [];
+    this.rrQuantumRestante = 0;
   }
 
   tick() {
@@ -16,6 +19,9 @@ export class Planificador {
         const asignado = this.memoria.asignar(p);
         if (asignado) {
           p.estado = Estado.LISTO;
+          if (this.algoritmo === 'RR') {
+            this.rrCola.push(p);
+          }
         }
       }
     }
@@ -23,20 +29,39 @@ export class Planificador {
     // 2. Ejecutar proceso actual
     if (this.enEjecucion) {
       this.enEjecucion.tick(this.tActual);
+      this.rrQuantumRestante--;
+
       if (this.enEjecucion.estado === Estado.TERMINADO) {
         this.memoria.liberar(this.enEjecucion);
+        this.enEjecucion = null;
+      } else if (this.algoritmo === 'RR' && this.rrQuantumRestante <= 0) {
+        this.enEjecucion.estado = Estado.LISTO;
+        this.rrCola.push(this.enEjecucion);
         this.enEjecucion = null;
       }
     }
 
     // 3. Seleccionar nuevo proceso si no hay uno en ejecución
     if (!this.enEjecucion) {
-      const candidatos = this.procesos.filter(p => p.estado === Estado.LISTO);
+      let candidatos = this.procesos.filter(p => p.estado === Estado.LISTO);
+
       if (this.algoritmo === 'SJF') {
         candidatos.sort((a, b) => a.restante - b.restante);
+        if (candidatos.length > 0) {
+          this.enEjecucion = candidatos[0];
+        }
+      } else if (this.algoritmo === 'RR') {
+        while (this.rrCola.length > 0) {
+          const candidato = this.rrCola.shift();
+          if (candidato.estado === Estado.LISTO) {
+            this.enEjecucion = candidato;
+            break;
+          }
+        }
+        this.rrQuantumRestante = this.quantum;
       }
-      if (candidatos.length > 0) {
-        this.enEjecucion = candidatos[0];
+
+      if (this.enEjecucion) {
         this.enEjecucion.estado = Estado.EJECUTANDO;
       }
     }
